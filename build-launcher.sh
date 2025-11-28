a069cc4a3b1349a3f5199a61de1ec4e39e4e815a
high on tantor <josephvenable@verizon.net>
2025-11-27 21:54:43 -0500

    Update build-launcher.sh

--- build-launcher.sh
+++ build-launcher.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd "${0%/*}";
-"C:/Windows/System32/cmd.exe" /c "../msys64/msys2_shell.cmd" -defterm -mingw32 -no-start -here -lc "./STIGMA.sh";
+"../msys64/msys2_shell.cmd" -defterm -mingw32 -no-start -here -lc "./STIGMA.sh";
