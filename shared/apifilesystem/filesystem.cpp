2276491536c061bab2f2db7923d7337da9b3db35
high on tantor <josephvenable@verizon.net>
2026-05-21 02:58:44 -0400

    Update filesystem.cpp

--- shared/apifilesystem/filesystem.cpp
+++ shared/apifilesystem/filesystem.cpp
@@ -24,13 +24,26 @@
  
 */
 
-#include <set>
+
+#pragma once
+#if ((defined(_WIN32) || defined(_WIN64)) || (defined(__APPLE__) && defined(__MACH__)) || (defined(__linux__) || defined(__ANDROID__)) || (defined(__FreeBSD__) || defined(__FreeBSD_kernel__)) || defined(__DragonFly__) || defined(__NetBSD__) || defined(__OpenBSD__) || (defined(__sun) && defined(__SVR4)))
+#if (defined(__APPLE__) && defined(__MACH__))
+#include <TargetConditionals.h>
+#if (!defined(TARGET_OS_OSX) || !TARGET_OS_OSX)
+#error "Unsupported Platform! Supported Platforms: Windows, macOS, Linux, FreeBSD, DragonFly BSD, NetBSD, OpenBSD, Solaris, illumos, and Android."
+#endif
+#else
+#error "Unsupported Platform! Supported Platforms: Windows, macOS, Linux, FreeBSD, DragonFly BSD, NetBSD, OpenBSD, Solaris, illumos, and Android."
+#endif
+#endif
+#if ((defined(_WIN32) || defined(_WIN64)) || (defined(__APPLE__) && defined(__MACH__)) || (defined(__linux__) || defined(__ANDROID__)) || (defined(__FreeBSD__) || defined(__FreeBSD_kernel__)) || defined(__DragonFly__) || defined(__NetBSD__) || defined(__OpenBSD__) || (defined(__sun) && defined(__SVR4)))
+#include <algorithm>
 #include <sstream>
 #include <fstream>
-#include <algorithm>
 #include <vector>
 #include <random>
 #include <thread>
+#include <set>
 
 #include <climits>
 #include <cstdlib>
@@ -1738,3 +1751,4 @@ namespace ngs::fs {
   }
 
 } // namespace ngs::fs
+#endif
