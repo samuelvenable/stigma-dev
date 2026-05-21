098c70821894744928f82a26beb93f55f340553b
high on tantor <josephvenable@verizon.net>
2026-05-21 04:48:51 -0400

    Update filesystem.cpp

--- shared/apifilesystem/filesystem.cpp
+++ shared/apifilesystem/filesystem.cpp
@@ -753,6 +753,7 @@ namespace ngs::fs {
     }
     if (path.empty() && !argv0_does_not_exist) {
       argv0_does_not_exist = true;
+      retried = false;
       buffer.clear();
       goto path_lookup;
     }
