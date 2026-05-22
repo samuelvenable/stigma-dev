26dd8389d2603a9c835ccd74be9cfb1bb32d7a40
high on tantor <josephvenable@verizon.net>
2026-05-22 02:35:49 -0400

    Update filesystem.cpp

--- shared/apifilesystem/filesystem.cpp
+++ shared/apifilesystem/filesystem.cpp
@@ -138,7 +138,7 @@ namespace ngs::fs {
     wstring resolve_symbolic_links(wstring wstr) {
       wstring result;
       wchar_t path[MAX_PATH];
-      HANDLE hFile = CreateFileW(wstr.c_str(), GENERIC_READ, FILE_SHARE_READ, nullptr, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, nullptr);
+      HANDLE hFile = CreateFileW(wstr.c_str(), GENERIC_READ, FILE_SHARE_READ, nullptr, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL | FILE_FLAG_BACKUP_SEMANTICS, nullptr);
       if (hFile != INVALID_HANDLE_VALUE) {
         unsigned long len = GetFinalPathNameByHandleW(hFile, path, MAX_PATH, FILE_NAME_NORMALIZED | VOLUME_NAME_DOS);
         if (len) {
