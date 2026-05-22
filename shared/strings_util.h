f4bd58776e393bd642658755f3e05f8bcd1a7f6e
high on tantor <josephvenable@verizon.net>
2026-05-21 21:56:38 -0400

    Update strings_util.h

--- shared/strings_util.h
+++ shared/strings_util.h
@@ -18,24 +18,24 @@
 
 namespace strings_util {
 
-  inline std::wstring widen(std::string str) {
-    if (str.empty()) return L"";
-    size_t wchar_count = str.size() + 1;
-    std::vector<wchar_t> buf(wchar_count);
-    wchar_count = (size_t)MultiByteToWideChar(CP_UTF8, 0, str.c_str(), -1, buf.data(), (int)wchar_count);
-    if (!wchar_count) return L"";
-    return std::wstring { buf.data(), wchar_count };
-  }
+inline std::wstring widen(std::string str) {
+  if (str.empty()) return L"";
+  size_t wchar_count = str.size() + 1;
+  std::vector<wchar_t> buf(wchar_count);
+  wchar_count = (size_t)MultiByteToWideChar(CP_UTF8, 0, str.c_str(), -1, buf.data(), (int)wchar_count);
+  if (!wchar_count) return L"";
+  return std::wstring { buf.data(), wchar_count };
+}
 
-  inline std::string shorten(std::wstring str) {
-    if (str.empty()) return "";
-    int nbytes = WideCharToMultiByte(CP_UTF8, 0, str.c_str(), (int)str.length(), nullptr, 0, nullptr, nullptr);
-    if (!nbytes) return "";
-    std::vector<char> buf((size_t)nbytes);
-    nbytes = WideCharToMultiByte(CP_UTF8, 0, str.c_str(), (int)str.length(), buf.data(), nbytes, nullptr, nullptr);
-    if (!nbytes) return "";
-    return std::string { buf.data(), (size_t)nbytes };
-  }
+inline std::string shorten(std::wstring str) {
+  if (str.empty()) return "";
+  int nbytes = WideCharToMultiByte(CP_UTF8, 0, str.c_str(), (int)str.length(), nullptr, 0, nullptr, nullptr);
+  if (!nbytes) return "";
+  std::vector<char> buf((size_t)nbytes);
+  nbytes = WideCharToMultiByte(CP_UTF8, 0, str.c_str(), (int)str.length(), buf.data(), nbytes, nullptr, nullptr);
+  if (!nbytes) return "";
+  return std::string { buf.data(), (size_t)nbytes };
+}
 
 } // namespace strings_util
 
