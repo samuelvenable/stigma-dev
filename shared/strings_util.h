5b2bdd43e14b5a09d8e8ca8ca00d5a0342084e6b
high on tantor <josephvenable@verizon.net>
2026-05-21 21:44:25 -0400

    Update strings_util.h

--- shared/strings_util.h
+++ shared/strings_util.h
@@ -18,19 +18,24 @@
 
 namespace strings_util {
 
-inline std::wstring widen(const std::string &str) {
-  if (str.empty()) return L"";
-  const size_t wchar_count = str.size() + 1;
-  std::vector<WCHAR> buf(wchar_count);
-  return std::wstring{buf.data(), (size_t)MultiByteToWideChar(CP_UTF8, 0, str.c_str(), -1, buf.data(), (int)wchar_count)};
-}
+  inline std::wstring widen(std::string str) {
+    if (str.empty()) return L"";
+    std::size_t wchar_count = str.size() + 1;
+    std::vector<wchar_t> buf(wchar_count);
+    wchar_count = (std::size_t)MultiByteToWideChar(CP_UTF8, 0, str.c_str(), -1, buf.data(), (int)wchar_count);
+    if (!wchar_count) return L"";
+    return std::wstring { buf.data(), wchar_count };
+  }
 
-inline std::string shorten(std::wstring str) {
-  if (str.empty()) return "";
-  int nbytes = WideCharToMultiByte(CP_UTF8, 0, str.c_str(), (int)str.length(), NULL, 0, NULL, NULL);
-  std::vector<char> buf((size_t)nbytes);
-  return std::string{buf.data(), (size_t)WideCharToMultiByte(CP_UTF8, 0, str.c_str(), (int)str.length(), buf.data(), nbytes, nullptr, nullptr)};
-}
+  inline std::string shorten(std::wstring wstr) {
+    if (wstr.empty()) return "";
+    int nbytes = WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), (int)wstr.length(), nullptr, 0, nullptr, nullptr);
+    if (!nbytes) return "";
+    std::vector<char> buf((std::size_t)nbytes);
+    nbytes = WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), (int)wstr.length(), buf.data(), nbytes, nullptr, nullptr);
+    if (!nbytes) return "";
+    return std::string { buf.data(), (std::size_t)nbytes };
+  }
 
 } // namespace strings_util
 
