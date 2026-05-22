bec85c09ce3b65a0ab333413f4374b0d263324e7
high on tantor <josephvenable@verizon.net>
2026-05-22 05:44:15 -0400

    Update estring.h

--- ENIGMAsystem/SHELL/Universal_System/estring.h
+++ ENIGMAsystem/SHELL/Universal_System/estring.h
@@ -29,7 +29,7 @@
 #include <cwchar>
 
 typedef std::basic_string<wchar_t> tstring;
-tstring widen(std::string str);
+tstring widen(const std::string &str);
 std::string shorten(tstring str);
 
 #endif
