bd00aa7070274401c77d4cb784e36ea93f586a42
high on tantor <josephvenable@verizon.net>
2026-05-22 20:51:53 -0400

    Update filesystem.cpp

--- shared/apifilesystem/filesystem.cpp
+++ shared/apifilesystem/filesystem.cpp
@@ -717,7 +717,7 @@ namespace ngs::fs {
           }
           goto retry;
         }
-        if (path.empty() && !argv0_does_not_exist && !leading_dash_removed && buffer.length() > 1 && buffer[0] == '-') {
+        if (path.empty() && !argv0_does_not_exist && !leading_dash_removed && slash_pos == string::npos && buffer.length() > 1 && buffer[0] == '-') {
           buffer = buffer.substr(1);
           retried = false;
           leading_dash_removed = true;
