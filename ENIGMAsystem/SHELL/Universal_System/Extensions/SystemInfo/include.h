c756c27ef294c31a0c006c028e13207afa588161
high on tantor <josephvenable@verizon.net>
2026-06-07 00:12:41 -0400

    Update include.h

--- ENIGMAsystem/SHELL/Universal_System/Extensions/SystemInfo/include.h
+++ ENIGMAsystem/SHELL/Universal_System/Extensions/SystemInfo/include.h
@@ -2,7 +2,7 @@
 
  MIT License
  
- Copyright © 2023-2025 Samuel Venable
+ Copyright © 2023-2026 Samuel Venable
  
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
@@ -29,83 +29,83 @@
 namespace enigma_user {
 
 inline std::string os_kernel_name() {
-  return ngs::sys::os_kernel_name();
+  return ngs::si::os_kernel_name();
 }
 
 inline std::string os_device_name() {
-  return ngs::sys::os_device_name();
+  return ngs::si::os_device_name();
 }
 
 inline std::string os_kernel_release() {
-  return ngs::sys::os_kernel_release();
+  return ngs::si::os_kernel_release();
 }
 
 inline std::string os_product_name() {
-  return ngs::sys::os_product_name();
+  return ngs::si::os_product_name();
 }
 
 inline std::string os_kernel_version() {
-  return ngs::sys::os_kernel_version();
+  return ngs::si::os_kernel_version();
 }
 
 inline std::string os_architecture() {
-  return ngs::sys::os_architecture();
+  return ngs::si::os_architecture();
 }
 
 inline std::string os_is_virtual() {
-  return ngs::sys::os_is_virtual();
+  return ngs::si::os_is_virtual();
 }
 
 inline std::string memory_totalram(bool hr) {
-  return ngs::sys::memory_totalram(hr);
+  return ngs::si::memory_totalram(hr);
 }
 
 inline std::string memory_freeram(bool hr) {
-  return ngs::sys::memory_freeram(hr); 
+  return ngs::si::memory_freeram(hr); 
 }
 
 inline std::string memory_usedram(bool hr) {
-  return ngs::sys::memory_usedram(hr);
+  return ngs::si::memory_usedram(hr);
 }
 
 inline std::string memory_totalswap(bool hr) {
-  return ngs::sys::memory_totalswap(hr);
+  return ngs::si::memory_totalswap(hr);
 }
 
 inline std::string memory_freeswap(bool hr) {
-  return ngs::sys::memory_freeswap(hr);
+  return ngs::si::memory_freeswap(hr);
 }
 
 inline std::string memory_usedswap(bool hr) {
-  return ngs::sys::memory_usedswap(hr);
+  return ngs::si::memory_usedswap(hr);
 }
 
 inline std::string memory_totalvram(bool hr) {
-  return ngs::sys::memory_totalvram(hr);
+  return ngs::si::memory_totalvram(hr);
 }
   
 inline std::string gpu_manufacturer() {
-  return ngs::sys::gpu_manufacturer();
+  return ngs::si::gpu_manufacturer();
 }
 
 inline std::string gpu_renderer() {
-  return ngs::sys::gpu_renderer();
+  return ngs::si::gpu_renderer();
 }
 
 inline std::string cpu_vendor() {
-  return ngs::sys::cpu_vendor();
+  return ngs::si::cpu_vendor();
 }
 
 inline std::string cpu_processor() {
-  return ngs::sys::cpu_processor();
+  return ngs::si::cpu_processor();
 }
 
 inline std::string cpu_processor_count() {
-  return ngs::sys::cpu_processor_count();
+  return ngs::si::cpu_processor_count();
 }
 
 inline std::string cpu_core_count() {
-  return ngs::sys::cpu_core_count();
+  return ngs::si::cpu_core_count();
 }
 
 } // namespace enigma_user
