f0e45e50c1cb998c56bb9b48014f0e199b8610d9
high on tantor <josephvenable@verizon.net>
2026-07-18 04:56:55 -0400

    Update PFsystem.h

--- ENIGMAsystem/SHELL/Platforms/General/PFsystem.h
+++ ENIGMAsystem/SHELL/Platforms/General/PFsystem.h
@@ -23,67 +23,6 @@ using std::string;
 
 namespace enigma_user {
 
-#if !defined(os_unknown)
-#define os_unknown -1
-#endif
-#if !defined(os_win32)
-#define os_win32 0
-#endif
-#if !defined(os_win64)
-#define os_win64 1
-#endif
-#if !defined(os_macosx)
-#define os_macosx 2
-#endif
-#if !defined(os_linux)
-#define os_linux 3
-#endif
-#if !defined(os_freebsd)
-#define os_freebsd 4
-#endif
-#if !defined(os_dragonfly)
-#define os_dragonfly 5
-#endif
-#if !defined(os_netbsd)
-#define os_netbsd 6
-#endif
-#if !defined(os_openbsd)
-#define os_openbsd 7
-#endif
-#if !defined(os_sunos)
-#define os_sunos 8
-#endif
-#if (defined(_WIN32) && !defined(_WIN64) && !defined(os_windows))
-#define os_windows os_win32
-#elif (defined(_WIN32) && defined(_WIN64) && !defined(os_windows))
-#define os_windows os_win64
-#elif !defined(os_windows)
-#define os_windows os_unknown
-#endif
-#if !defined(os_type)
-#if (defined(_WIN32) && !defined(_WIN64))
-#define os_type os_win32
-#elif (defined(_WIN32) && defined(_WIN64))
-#define os_type os_win64
-#elif (defined(__APPLE__) && defined(__MACH__))
-#define os_type os_macosx
-#elif defined(__linux__)
-#define os_type os_linux
-#elif defined(__FreeBSD__)
-#define os_type os_freebsd
-#elif defined(__DragonFly__)
-#define os_type os_dragonfly
-#elif defined(__NetBSD__)
-#define os_type os_netbsd
-#elif defined(__OpenBSD__)
-#define os_type os_openbsd
-#elif defined(__sun)
-#define os_type os_sunos
-#else
-#define os_type os_unknown
-#endif
-#endif
-
 string os_get_config();
 int os_get_info();
 string os_get_language();
