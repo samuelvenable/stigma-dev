ab22bd44ceafd99ab96a7256f921058b486bdb76
high on tantor <josephvenable@verizon.net>
2026-07-18 00:34:19 -0400

    Update platforms_mandatory.h

--- ENIGMAsystem/SHELL/Platforms/platforms_mandatory.h
+++ ENIGMAsystem/SHELL/Platforms/platforms_mandatory.h
@@ -39,7 +39,7 @@ typedef void * window_t;
 typedef std::string wid_t;
 
 namespace enigma_user {
-  extern int os_type;
+  extern const int os_type;
   extern unsigned long current_time; // milliseconds since the start of the game
   extern unsigned long delta_time; // microseconds since the last step event
 }
