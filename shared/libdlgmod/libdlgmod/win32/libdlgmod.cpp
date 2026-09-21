7a426bc3b1d4a73095fcfed5984dadacff85ff7b
high on tantor <josephvenable@verizon.net>
2026-09-21 15:17:34 -0400

    Update libdlgmod.cpp

--- shared/libdlgmod/libdlgmod/win32/libdlgmod.cpp
+++ shared/libdlgmod/libdlgmod/win32/libdlgmod.cpp
@@ -1,26 +1,26 @@
 /*
 
- MIT License
+MIT License
 
- Copyright © 2021-2026 Samuel Venable
+Copyright © 2021-2026 Samuel Venable
 
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
- The above copyright notice and this permission notice shall be included in all
- copies or substantial portions of the Software.
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
 
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- SOFTWARE.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 
 */
 
@@ -496,7 +496,7 @@ namespace dialog_module {
             Gdiplus::GdiplusStartupInput gdiplusStartupInput;
             Gdiplus::GdiplusStartup(&m_gdiplusToken, &gdiplusStartupInput, nullptr);
             Bitmap *png = Bitmap::FromFile(cpp_wstr_icon.c_str());
-           png->GetHICON(&hIcon);
+            png->GetHICON(&hIcon);
             PostMessage(dlg, WM_SETICON, ICON_SMALL, (LPARAM)hIcon);
             delete png;
             Gdiplus::GdiplusShutdown(m_gdiplusToken);
@@ -667,7 +667,7 @@ namespace dialog_module {
       if (!fp) { return ""; }
       Evaluation = "WScript.Echo " + Evaluation;
       std::size_t result = fwrite(Evaluation.data(), sizeof(char), Evaluation.length(), fp);
-      if (result < Evaluation.length()) { fclose(fp); if (o && IsWindow(o)) { return ""; }
+      if (result < Evaluation.length()) { fclose(fp); return ""; }
       else { fclose(fp); }
       MoveFileW(wbuff, (wbuff + wstring(L".vbs")).c_str());
       apiprocess::proc_id_t proc_id = apiprocess::spawn_child_proc_id((string("cscript.exe /nologo \"") + narrow(wbuff) + string(".vbs\"")).c_str(), false);
